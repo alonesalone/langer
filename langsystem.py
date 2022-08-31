6f9f9ba2d64979893b7c501f1920e70fd1cfe190
alonesalone <40493307+alonesalone@users.noreply.github.com>
2022-08-31 05:54:45 +0300

    Update langsystem.py

--- langsystem.py
+++ langsystem.py
@@ -8,6 +8,7 @@ class Language:
     def __init__(self):
         self.filePath = ""
         self.tempwordlist = []
+        self.orderWords_list = None
 
     def getFile(self):
         if self.filePath:
@@ -20,6 +21,8 @@ class Language:
             get_dict = json.loads(w_list.read())
             self.words_list = get_dict[0]
             self.questions = get_dict[1]
+        
+        self.orderWords_list = self.words_list.copy()
 
     def randomChoice(self):
         k, v = random.choice(list(self.words_list.items()))
@@ -27,3 +30,17 @@ class Language:
         rastgelekelime = random.choice(self.tempwordlist)
 
         return rastgelekelime
+
+    def choiceInOrder(self):        
+        self.ordertemplist = []
+
+        oK, oV = random.choice(list(self.orderWords_list.items()))
+        self.ordertemplist.extend([oK, oV])
+        randomorder = random.choice(self.ordertemplist)
+
+        self.orderWords_list.pop(randomorder) if randomorder in self.orderWords_list else self.orderWords_list.pop(
+            list(self.orderWords_list.keys())[list(self.orderWords_list.values()).index(randomorder)]
+        )
+
+        return randomorder
+        
