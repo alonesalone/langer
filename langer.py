12d7b1f9dda3b74ea3b799c129e6a3ffd1dc49b5
alonesalone <40493307+alonesalone@users.noreply.github.com>
2022-08-31 05:55:26 +0300

    Update langer.py

--- langer.py
+++ langer.py
@@ -1,11 +1,12 @@
 from PyQt5 import QtCore, QtWidgets
-from Pages import Ui_MainWindow, Ui_new_window, Ui_after_question
+from Pages import Ui_MainWindow, Ui_new_window, Ui_after_question, Ui_order_over
 from langsystem import Language
 
 class LangerProgram(QtWidgets.QMainWindow):
     def __init__(self):
         super(LangerProgram, self).__init__()
         self.mainWindow()
+        self.lang = Language()
 
         self.ui.main_menu.triggered.connect(lambda: self.mainWindow())
 
@@ -16,12 +17,17 @@ class LangerProgram(QtWidgets.QMainWindow):
         self.ui.browseFilesBtn.hide()
 
         self.challenge = False
+        self.inOrder = False
 
         self.ui.go_practice.clicked.connect(self.pushed_go_practice)
         self.ui.radioTime.toggled.connect(self.isOn)
         self.ui.input_seconds.returnPressed.connect(self.pushed_go_practice)
         self.ui.select_combobox.currentIndexChanged.connect(self.btnBrowseFiles)
         self.ui.browseFilesBtn.clicked.connect(self.browseFiles)
+        self.ui.inOrderBtn.stateChanged.connect(self.inOrderChecked)
+
+        self.opened = False
+        
         self.filePath = ""
 
         try:
@@ -41,6 +47,11 @@ class LangerProgram(QtWidgets.QMainWindow):
                 self.pushed_go_practice()
                 return True
 
+        elif event.type() == QtCore.QEvent.KeyPress and obj is self.order_over.return_to_mmenu:
+            if event.key() == QtCore.Qt.Key_Return and self.order_over.return_to_mmenu.hasFocus():
+                self.mainWindow()
+                return True
+
         return False
 
     def btnBrowseFiles(self, index):
@@ -50,6 +61,12 @@ class LangerProgram(QtWidgets.QMainWindow):
         self.filePath, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Single File', QtCore.QDir.rootPath() , '*.json')
         fileName = self.filePath.split("/")[-1]
         self.ui.select_combobox.setItemText(1, fileName) if self.filePath else self.ui.select_combobox.setItemText(1, "Özel")
+
+    def inOrderChecked(self):
+        if self.ui.inOrderBtn.isChecked():
+            self.inOrder = True
+        else:
+            self.inOrder = False
 
     def isOn(self):
         if self.sender().isChecked():
@@ -84,12 +101,31 @@ class LangerProgram(QtWidgets.QMainWindow):
         self.newPage.main_menu.triggered.connect(lambda: self.mainWindow())
         self.newPage.remove_timer.triggered.connect(lambda: self.remove_timer())
 
-
-        self.lang = Language()
         self.lang.filePath = self.filePath
-        self.lang.openFile()
+        
+        if not self.opened:
+            self.lang.openFile()
+        else:
+            pass
+        
+        print(self.lang.words_list)
 
-        self.new_word = self.lang.randomChoice()
+        if not self.inOrder:
+            self.new_word = self.lang.randomChoice()
+        
+        elif self.inOrder:
+            if len(list(self.lang.orderWords_list.keys())) > 0:
+                self.new_word = self.lang.choiceInOrder()
+                print(self.new_word)
+                print(self.lang.orderWords_list)
+            
+            else:
+                self.order_over = Ui_order_over()
+                self.order_over.setupUi(self)
+                self.order_over.return_to_mmenu.setFocus()
+                self.order_over.return_to_mmenu.clicked.connect(self.mainWindow)
+
+                self.order_over.over_text.setText(f"""<html><head/><body><p align=\"center\"><span style=\" font-size:20pt; font-weight:600;\">{len(list(self.lang.words_list.keys()))} adet kelime çalışması</span></p><p align=\"center\"><span style=\" font-size:20pt; font-weight:600;\">sona erdi!</span></p></body></html>""")
 
         above_text = """<html><head/><body><p align=\"center\"><span style=\" font-size:11pt; font-weight:600; font-style:italic;\">{}</span></p></body></html>"""
         self.newPage.above_text.setText(above_text.format(self.lang.questions["lang2"] if self.new_word in self.lang.words_list else self.lang.questions["lang1"]))
@@ -99,12 +135,14 @@ class LangerProgram(QtWidgets.QMainWindow):
 
         self.newPage.go_check_btn.clicked.connect(self.pushed_go)
         
+        
         try:
             self.challenge_seconds = self.ui.input_seconds.text()
         
         except:
             pass
 
+        
         if str(self.challenge_seconds).isnumeric():
             if int(self.challenge_seconds) <= self.max_sec and int(self.challenge_seconds) > 0:
                 self.challenge_seconds = int(self.challenge_seconds)
@@ -133,6 +171,7 @@ class LangerProgram(QtWidgets.QMainWindow):
             except:
                 pass
 
+
             try:
                 if self.temp_seconds == 0 and not self.newPage.go_check_btn.isChecked():
                     self.pushed_go()
@@ -141,6 +180,7 @@ class LangerProgram(QtWidgets.QMainWindow):
                 pass
 
     def pushed_go(self):
+        self.opened = True
 
         try:
             self.input_word = self.newPage.input_text.toPlainText()
@@ -160,11 +200,13 @@ class LangerProgram(QtWidgets.QMainWindow):
             self.afterQuestionPage.trueorfalse.setText("""<html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">Süren sona erdi.</span></p></body></html>""")
         
         else:
+
             self.afterQuestionPage.trueorfalse.setText(f"""
-            <html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">{"Doğru cevap" 
+                <html><head/><body><p align=\"center\"><span style=\" font-size:12pt;\">{"Doğru cevap" 
             if (self.new_word in self.lang.words_list and self.input_word == self.lang.words_list[self.new_word]) or (self.new_word not in self.lang.words_list and self.input_word == list(self.lang.words_list.keys())[list(self.lang.words_list.values()).index(self.new_word)]) 
             else 
-            "Yanlış cevap."}</span></p></body></html>""")
+                "Yanlış cevap."}</span></p></body></html>""")
+
 
         lang_2_text = "<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; font-weight:600;\">{}</span></p></body></html>"
         self.afterQuestionPage.lang_2.setText(lang_2_text.format(self.lang.words_list[self.new_word].capitalize() if self.new_word in self.lang.words_list else list(self.lang.words_list.keys())[list(self.lang.words_list.values()).index(self.new_word)].capitalize()))
